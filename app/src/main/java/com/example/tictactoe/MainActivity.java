783876412a49e31afff78b62ccaa24685ffd0662
Nomaan <nomaanhusain@gmail.com>
2020-05-11 15:34:10 +0530

    Tic Tac Toe

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,12 +3,108 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button[][] buttons=new Button[3][3];
+    private boolean player1turn=true;
+    private int roundCount;
+    private int player1points;
+    private int player2points;
+    private TextView textViewplayer1;
+    private TextView textViewplayer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewplayer1=findViewById(R.id.text_view_p1);
+        textViewplayer2=findViewById(R.id.text_view_p2);
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                String buttonId="button_"+i+j;
+                int resId=getResources().getIdentifier(buttonId,"id",getPackageName());
+                buttons[i][j]=findViewById(resId);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+        Button button_reset=findViewById(R.id.button_reset);
+        button_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")){
+            return;
+        }
+        if (player1turn){
+            ((Button) v).setText("X");
+        }
+        else{
+            ((Button) v).setText("O");
+        }
+         roundCount++;
+        if (checkWin()){
+            if(player1turn){
+                player1Wins();
+            }
+            else {
+                player2wins();
+            }
+            else if(roundCount == 9){
+                draw();
+            }else {
+                player1turn=!player1turn;
+            }
+        }
+    }
+    private boolean checkWin(){
+        String[][] field=new String[3][3];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                field[i][j]=buttons[i][j].getText().toString();
+            }
+        }
+        //vertical check
+        for (int i=0;i<3;i++){
+            if (field[i][0].equals(field[i][1])&& field[i][0].equals(field[i][2])&& !field[i][0].equals("")){
+                return true;
+            }
+        }
+        //horizontal Check
+        for (int i=0;i<3;i++){
+            if (field[0][i].equals(field[1][i])&& field[0][i].equals(field[2][i])&& !field[0][i].equals("")){
+                return true;
+            }
+        }
+        //Diagonal left to right
+
+        if (field[0][0].equals(field[1][1])&& field[0][0].equals(field[2][2])&& !field[0][0].equals("")){
+            return true;
+        }
+        //Diagonal right to left
+
+        if (field[0][2].equals(field[1][1])&& field[0][2].equals(field[2][0])&& !field[0][2].equals("")){
+            return true;
+        }
+        return false;
+    }
+
+    private void player1Wins(){
+
+    }
+    private void player2wins(){
+
+    }
+    private void draw(){
+
     }
 }
